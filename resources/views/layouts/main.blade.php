bf84c9e811243c429b951af39e2b79ceb6f257e1
prukon <prukon@gmail.com>
2023-11-15 00:58:57 +0300

    lesson 16. Add bootstrap nav

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,19 +6,44 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="{{public_path('https://github.com/prukon/laravel/blob/b56054b7096317a3c40b1f39c41691c98d9f386d/node_modules/bootstrap/scss/bootstrap.scss')}}">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+
 </head>
 <body>
-<div>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a></li>
-                <li><a href="{{route('post.index')}}">Posts</a></li>
-                <li><a href="{{route('about.index')}}">About</a></li>
-                <li><a href="{{route('contact.index')}}">Contacts</a></li>
-            </ul>
+<div class="container">
+    <div class="row">
+        
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('post.index')}}">Post</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('about.index')}}">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('contact.index')}}">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
+
+{{--        <nav>--}}
+{{--            <ul>--}}
+{{--                <li><a href="{{route('main.index')}}">Main</a></li>--}}
+{{--                <li><a href="{{route('post.index')}}">Posts</a></li>--}}
+{{--                <li><a href="{{route('about.index')}}">About</a></li>--}}
+{{--                <li><a href="{{route('contact.index')}}">Contacts</a></li>--}}
+{{--            </ul>--}}
+{{--        </nav>--}}
     </div>
     @yield('content')
 </div>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,33 @@
 @extends('layouts/main')
 
 @section('content')
-    <div>
-        this is post
-    </div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Имя</th>
+            <th scope="col">Фамилия</th>
+            <th scope="col">Обращение</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
 @endsection
